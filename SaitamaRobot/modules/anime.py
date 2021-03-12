9a1277f22f88b389f28aac6eb1abde98da6dad3e
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 19:51:52 +0700

    Update anime.py

--- SaitamaRobot/modules/anime.py
+++ SaitamaRobot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SaitamaRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SpongebobBot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from SpongebobBot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
