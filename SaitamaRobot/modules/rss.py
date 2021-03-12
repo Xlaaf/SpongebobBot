eede6dd9be857b22c0ec8e029173ecf9090fbc54
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 20:24:15 +0700

    Update rss.py

--- SaitamaRobot/modules/rss.py
+++ SaitamaRobot/modules/rss.py
@@ -2,9 +2,9 @@ import html
 import re
 
 from feedparser import parse
-from SaitamaRobot import dispatcher, updater
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.sql import rss_sql as sql
+from SpongebobBot import dispatcher, updater
+from SpongebobBot.modules.helper_funcs.chat_status import user_admin
+from SpongebobBot.modules.sql import rss_sql as sql
 from telegram import ParseMode, Update, constants
 from telegram.ext import CallbackContext, CommandHandler
 
