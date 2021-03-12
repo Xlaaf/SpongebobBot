85cf0aadb00e826c781cb9529a904f6ef42df15f
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 22:46:23 +0700

    Update chat_status.py

--- SaitamaRobot/modules/helper_funcs/chat_status.py
+++ SaitamaRobot/modules/helper_funcs/chat_status.py
@@ -409,6 +409,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SaitamaRobot.modules import connection
+from SpongebobBot.modules import connection
 
 connected = connection.connected
