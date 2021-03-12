0977f30b89c8e613ab3b074f6bc0c55bf722adc2
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 20:19:53 +0700

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from SaitamaRobot import StartTime, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SpongebobBot import StartTime, dispatcher
+from SpongebobBot.modules.helper_funcs.chat_status import sudo_plus
+from SpongebobBot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
