953efc442cb36bf4e82c2b6d473dbb7c27c9ec64
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 23:03:36 +0700

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} has gone to leave the krusty Krab 😔".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {} here you krabby!', 'Where is {}?\nIn the chat!'
+                '{} is here!', '{} is back! Patrick is waiting for you', '{} is now in the chat! He's got a girlfriend now!',
+                '{} is awake! Krabs are waiting for you', '{} is back online! You got Squidward?', '{} is finally here! No one misses you',
+                'Welcome back! {} here you krabby!', 'Where is {}?\nIn the chat! Let's cook krabbypatty'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
