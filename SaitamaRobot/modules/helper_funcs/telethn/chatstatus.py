d2d0dfb320de8496f97fc8c40ec410f92d0d2978
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 20:46:39 +0700

    Update chatstatus.py

--- SaitamaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ SaitamaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from SpongebobBot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from SpongebobBot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
