92520c37ebed714400ec7d7434a156eba2cfaea7
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 20:27:49 +0700

    Update speed_test.py

--- SaitamaRobot/modules/speed_test.py
+++ SaitamaRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from SaitamaRobot import DEV_USERS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+from SpongebobBot import DEV_USERS, dispatcher
+from SpongebobBot.modules.disable import DisableAbleCommandHandler
+from SpongebobBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
