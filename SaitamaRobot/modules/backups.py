17aabd1a2eca0d7c64d63f3d8dc9fec970761245
Xlaaf <80510724+Xlaaf@users.noreply.github.com>
2021-03-12 19:55:37 +0700

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -5,23 +5,23 @@ from telegram import ParseMode, Message
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER
-from SaitamaRobot.__main__ import DATA_IMPORT
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.alternate import typing_action
+import SpongebobBot.modules.sql.notes_sql as sql
+from SpongebobBot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER
+from SpongebobBot.__main__ import DATA_IMPORT
+from SpongebobBot.modules.helper_funcs.chat_status import user_admin
+from SpongebobBot.modules.helper_funcs.alternate import typing_action
 
-# from SaitamaRobot.modules.rules import get_rules
-import SaitamaRobot.modules.sql.rules_sql as rulessql
+# from SpongebobBot.modules.rules import get_rules
+import SpongebobBot.modules.sql.rules_sql as rulessql
 
-# from SaitamaRobot.modules.sql import warns_sql as warnssql
-import SaitamaRobot.modules.sql.blacklist_sql as blacklistsql
-from SaitamaRobot.modules.sql import disable_sql as disabledsql
+# from SpongebobBot.modules.sql import warns_sql as warnssql
+import SpongebobBot.modules.sql.blacklist_sql as blacklistsql
+from SpongebobBot.modules.sql import disable_sql as disabledsql
 
-# from SaitamaRobot.modules.sql import cust_filters_sql as filtersql
-# import SaitamaRobot.modules.sql.welcome_sql as welcsql
-import SaitamaRobot.modules.sql.locks_sql as locksql
-from SaitamaRobot.modules.connection import connected
+# from SpongebobBot.modules.sql import cust_filters_sql as filtersql
+# import SpongebobBot.modules.sql.welcome_sql as welcsql
+import SpongebobBot.modules.sql.locks_sql as locksql
+from SpongebobBot.modules.connection import connected
 
 
 @run_async
